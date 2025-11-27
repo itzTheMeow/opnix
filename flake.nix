ffce3d5fe4262fa2e2353409b4fec4ba6a2ff169
Meow <github@xela.codes>
2025-11-27 17:29:06 -0500

    hash mismatch?

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         pname = "opnix";
         version = "0.7.0";
         inherit src;
-        vendorHash = "sha256-rmwZue0X6o0q29ZVe9bWHBOxHVx/yiMJXHc4urooaHo=";
+        vendorHash = "sha256-Wd5oHFhDiWY8kjSW4iRN840PlYJ5lHlWb9gM1+Q9F90=";
         subPackages = ["cmd/opnix"];
       };
 
--- nix/package.nix
+++ nix/package.nix
@@ -3,6 +3,6 @@ pkgs.buildGoModule {
   pname = "opnix";
   version = "0.7.0";
   src = ../.;
-  vendorHash = "sha256-rmwZue0X6o0q29ZVe9bWHBOxHVx/yiMJXHc4urooaHo=";
+  vendorHash = "sha256-Wd5oHFhDiWY8kjSW4iRN840PlYJ5lHlWb9gM1+Q9F90=";
   subPackages = ["cmd/opnix"];
 }
